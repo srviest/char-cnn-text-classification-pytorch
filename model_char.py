91b0300c4ae1de96e3d1822de62e784fc8a21e2d
Yuan-Ping Chen <ychen@1687-MacBook-Pro.local>
2017-08-07 16:13:21 -0400

    first commit of CharCNN in pytorch

--- model_char.py
+++ model_char.py
@@ -9,46 +9,46 @@ class  CharCNN(nn.Module):
         self.args = args
         self.conv1 = nn.Sequential(
             nn.Conv1d(70, 256, kernel_size=7, stride=1),
-            nn.Threshold(),
+            nn.Threshold(1e-6, 0),
             nn.MaxPool1d(kernel_size=3, stride=3)
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=7, stride=1),
-            nn.Threshold(),
+            nn.Threshold(1e-6, 0),
             nn.MaxPool1d(kernel_size=3, stride=3)
         )
 
         self.conv3 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
-            nn.Threshold()
+            nn.Threshold(1e-6, 0)
         )
 
         self.conv4 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
-            nn.Threshold()
+            nn.Threshold(1e-6, 0)
         )
 
         self.conv5 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
-            nn.Threshold()
+            nn.Threshold(1e-6, 0)
         )
 
         self.conv6 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
-            nn.Threshold(),
+            nn.Threshold(1e-6, 0),
             nn.MaxPool1d(kernel_size=3, stride=3)
         )
 
         
         self.fc1 = nn.Sequential(
             nn.Linear(8704, 1024),
-            nn.Threshold(),
+            nn.Threshold(1e-6, 0),
             NN.Dropout(P=0.5)
         )
         self.fc2 = nn.Sequential(
             nn.Linear(1024, 1024),
-            nn.Threshold(),
+            nn.Threshold(1e-6, 0),
             NN.Dropout(P=0.5)
         )
         self.fc3 = nn.Sequential(
