6b59b2e04e01df95dca1a79e7c1048a5700cba8b
Yuan-Ping Chen <ychen@1687-MacBook-Pro.local>
2017-08-15 17:03:28 -0400

    refactor

--- model_CharCNN.py
+++ model_CharCNN.py
@@ -9,10 +9,12 @@ class  CharCNN(nn.Module):
         super(CharCNN, self).__init__()
         
         self.num_features = args.num_features
-        self.conv1 = nn.Conv1d(self.num_features, 256, kernel_size=7, stride=1)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool1d(kernel_size=3, stride=3)
-            
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(self.num_features, 256, kernel_size=7, stride=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=3, stride=3)
+        )
+
         self.conv2 = nn.Conv1d(256, 256, kernel_size=7, stride=1)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool1d(kernel_size=3, stride=3)
@@ -36,12 +38,12 @@ class  CharCNN(nn.Module):
         
         self.fc1 = nn.Linear(8704, 1024)
         self.relu7= nn.ReLU()
-        self.dropout7= nn.Dropout(p=0.5)
+        self.dropout7= nn.Dropout(p=args.dropout)
             
         
         self.fc2 = nn.Linear(1024, 1024)
         self.relu8= nn.ReLU()
-        self.dropout8= nn.Dropout(p=0.5)
+        self.dropout8= nn.Dropout(p=args.dropout)
             
         
         self.fc3 = nn.Linear(1024, 4)
@@ -51,9 +53,9 @@ class  CharCNN(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         # print('x.size()', x.size())
-        x = self.relu1(x)
+        # x = self.relu1(x)
         # print('x.size()', x.size())
-        x = self.pool1(x)
+        # x = self.pool1(x)
         # print('x.size()', x.size())
         x = self.conv2(x)
         x = self.relu2(x)
@@ -69,21 +71,18 @@ class  CharCNN(nn.Module):
         x = self.relu6(x)
         x = self.pool6(x)
 
-        # print('x.size()', x.size())
-        # print('x.size(0)', x.size(0))
         x = x.view(x.size(0), -1)
-        # print('Collapse x:, ', x.size())
+       
         x = self.fc1(x)
         x = self.relu7(x)
         x = self.dropout7(x)
-        # print('FC1: ', x.size())
+       
         x = self.fc2(x)
         x = self.relu8(x)
         x = self.dropout8(x)
-        # print('FC2: ', x.size())
+       
         x = self.fc3(x)
-        # print('x: ', x.size())
-        # x = self.inference_log_softmax(x)
+       
 
         x = self.softmax(x)
 
