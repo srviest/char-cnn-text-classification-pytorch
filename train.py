cf089b160100f4ba68497c1d9a6323b7842a4d00
Yuan-Ping Chen <qoo0972@hotmail.com>
2017-08-31 11:25:46 -0400

    debug

--- train.py
+++ train.py
@@ -120,17 +120,10 @@ def eval(data_loader, model, args):
         predicates_batch, target_batch = predicates.cpu().numpy().tolist(), target.data.cpu().numpy().tolist()
 
         batch_loss = loss.data[0]
-        print('loss.data[0]:')
-        print(loss.data[0])
         avg_loss += batch_loss
-        print('accumulated loss: ')
-        print(avg_loss)
         corrects += correct
-        print('number of data per batch:')
-        print(len(target))
         size+=len(target)
-        print('accumulated size: ')
-        print(size)
+
         predicates_all+=predicates_batch
         target_all+=target_batch
 
